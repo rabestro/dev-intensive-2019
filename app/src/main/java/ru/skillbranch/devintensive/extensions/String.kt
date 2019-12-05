9d5202017138985261d48b1db0e363748b81cd36
Yegor Chemisov <yegor.chemisov@gmail.com>
2019-12-05 20:11:42 +0200

    Added function plural #10

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,5 +1,9 @@
 package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(length : Int = 16) : String {
-    return this.padEnd(3,'.')
+    return this.padEnd(length,'.')
+}
+
+fun String.stripHtml() : String {
+    return ""
 }
