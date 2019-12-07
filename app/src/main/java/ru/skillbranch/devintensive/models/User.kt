27ee407558b2f9d30eb35e5d0fccbc9b55c9dc64
Yegor Chemisov <yegor.chemisov@gmail.com>
2019-12-07 16:05:00 +0200

    Added builder for class User. Issue #9

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -13,17 +13,70 @@ data class User (
     var isOnline:Boolean = false
     )
 {
-    var introBit:String
+    var introBit: String
 
-    constructor(id:String, firstName:String?, lastName:String?) : this(
+    constructor(id: String, firstName: String?, lastName: String?) : this(
         id = id,
         firstName = firstName,
         lastName = lastName,
         avatar = null
     )
 
-    constructor(id:String) : this(id, "John", "Doe")
+    constructor(id: String) : this(id, "John", "Doe")
 
+    private constructor(builder: Builder) : this(
+        builder.id,
+        builder.firstName,
+        builder.lastName,
+        builder.avatar,
+        builder.rating,
+        builder.respect,
+        builder.lastVisit,
+        builder.isOnline)
+
+    class Builder {
+        var id: String = ""
+            private set
+
+        var firstName: String? = null
+            private set
+
+        var lastName: String? = null
+            private set
+
+        var avatar: String? = null
+            private set
+
+        var rating: Int = 0
+            private set
+
+        var respect: Int = 0
+            private set
+
+        var lastVisit: Date? = Date()
+            private set
+
+        var isOnline: Boolean = false
+            private set
+
+        fun id(id: String) = apply {this.id = id }
+
+        fun firstName(firstName: String) = apply {this.firstName = firstName }
+
+        fun lastName(lastName: String) = apply {this.lastName = lastName }
+
+        fun avatar(avatar: String?) = apply {this.avatar = avatar }
+
+        fun rating(rating: Int) = apply {this.rating = rating }
+
+        fun respect(respect: Int) = apply {this.respect = respect }
+
+        fun lastVisit(lastVisit: Date) = apply {this.lastVisit = lastVisit }
+
+        fun isOnline(isOnline: Boolean) = apply {this.isOnline = isOnline }
+
+        fun build() = User(this)
+    }
     init {
         introBit = getIntro()
         println("It's Alive!\n${
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,7 +2,7 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName:String?):Pair<String?, String?>  {
-        val parts : List<String>? = fullName?.split(" ")
+        val parts : List<String>? = fullName?.trim()?.split(" ")
         val firstName:String? = parts?.getOrNull(0)
         val lastName = parts?.getOrNull(1)
 
