bcde30f9e69e52beadcf1303d97e8e9b09229c71
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2019-12-07 18:05:48 +0200

    Implemented stripHtml, issue #12

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -2,6 +2,4 @@ package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(length: Int = 16) = if (this.length > length) this.substring(0, length).trimEnd() + "..." else this
 
-fun String.stripHtml() : String {
-    return ""
-}
+fun String.stripHtml() = this.replace(Regex("<[^>]*>"|"&amp;|&lt;|&gt;|&quot;|&apos;|&#\\d+;"), "").replace(Regex(" +"), " ")
