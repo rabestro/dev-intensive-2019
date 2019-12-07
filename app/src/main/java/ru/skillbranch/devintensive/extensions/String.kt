0014231f6bc32b0365ab6900090abfcfdbf8e69e
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2019-12-07 17:54:35 +0200

    Implemented String.truncate. Issue #11

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,8 +1,6 @@
 package ru.skillbranch.devintensive.extensions
 
-fun String.truncate(length : Int = 16) : String {
-    return this.padEnd(length,'.')
-}
+fun String.truncate(length: Int = 16) = if (this.length > length) this.substring(0, length).trimEnd() + "..." else this
 
 fun String.stripHtml() : String {
     return ""
