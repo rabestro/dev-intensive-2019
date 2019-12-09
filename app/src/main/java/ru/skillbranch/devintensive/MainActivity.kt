f18398797c30115664954381b4af0b64cf20632e
Yegor Chemisov <yegor.chemisov@gmail.com>
2019-12-09 18:48:25 +0200

    Added dimens.xml

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -2,11 +2,15 @@ package ru.skillbranch.devintensive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.security.cert.LDAPCertStoreParameters
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity","")
     }
 }
