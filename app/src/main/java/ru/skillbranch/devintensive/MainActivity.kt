e5dd05eeba8f17f8709f3e78e97a298555c28b71
Yegor Chemisov <yegor.chemisov@gmail.com>
2019-12-09 23:11:01 +0200

    Added questions

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,17 +1,40 @@
 package ru.skillbranch.devintensive
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.models.Bender
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var benderImage : ImageView
+    lateinit var textTxt : TextView
+    lateinit var messageEt : EditText
+    lateinit var sendBtn : ImageView
+    lateinit var benderObj: Bender
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         Log.d("M_MainActivity","onCreate")
+
+        // benderImage = findViewById(R.id.iv_bender) as ImageView
+        benderImage = iv_bender
+        textTxt = tv_text
+        messageEt = et_message
+        sendBtn = iv_send
+
+        benderObj = Bender()
+
+        textTxt.text = benderObj.askQuestion()
+        sendBtn.setOnClickListener(this)
     }
 
     override fun onRestart() {
@@ -42,5 +65,15 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d("M_MainActivity","onDestroy")
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.iv_send) {
+            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
+            messageEt.setText("")
+            val (r, g, b) = color
+            benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+            textTxt.text = phrase
+        }
     }
 }
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -0,0 +1,41 @@
+package ru.skillbranch.devintensive.models
+
+class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
+
+    fun askQuestion() : String = when (question) {
+        Question.NAME -> Question.NAME.name
+        Question.PROFESSION -> Question.PROFESSION.name
+        Question.MATERIAL -> Question.MATERIAL.name
+        Question.BDAY -> Question.BDAY.name
+        Question.SERIAL -> Question.SERIAL.name
+        Question.IDLE -> Question.IDLE.name
+    }
+
+    fun listenAnswer(answer: String) : Pair<String, Triple<Int, Int, Int>> =
+        if (question.answers.contains(answer)) {
+            "Олично! Это правильный ответ!" to status.color
+        } else {
+            status = status.nextStatus()
+            "Это не правльный ответ!" to status.color
+        }
+
+
+    enum class Status(val color: Triple<Int, Int, Int>) {
+        NORMAL(Triple(255,255,255)),
+        WARNING(Triple(255,120,0)),
+        ERROR(Triple(255,60,60)),
+        CRITICAL(Triple(255,255,0)),
+
+        fun nextStatus() : Status = values()[
+                if (this.ordinal < values().lastIndex) this.ordinal + 1 else 0 ]
+    }
+
+    enum class Question(val question: String, val answers: List<String>) {
+        NAME("Как меня зовут?", listOf<String>("бендер", "bender")),
+        PROFESSION("Назови мою профессию?", listOf<String>("сгибальщик", "bender")),
+        MATERIAL("Из чего я сделан?", listOf<String>("метал", "дерево", "metal", "iron", "wood")),
+        BDAY("Когда меня создали?", listOf<String>("2993")),
+        SERIAL("Мой серийный номер?", listOf<String>("2716057")),
+        IDLE("На этом всё, вопросов больше нет.", listOf<String>()),
+    }
+}
