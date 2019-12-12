ac0e23f5187e2e83d27d4ad09702a1b0cb324c08
Yegor Chemisov <yegor.chemisov@gmail.com>
2019-12-12 15:06:01 +0200

    Fixed error in enum class Status

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -17,8 +17,8 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
 
         NORMAL(Triple(255,255,255)),
         WARNING(Triple(255,120,0)),
-        ERROR(Triple(255,60,60)),
-        CRITICAL(Triple(255,255,0));
+        DANGER(Triple(255,60,60)),
+        CRITICAL(Triple(255,0,0));
         fun nextStatus() : Status = values()[
                 if (this.ordinal < values().lastIndex) this.ordinal + 1 else 0 ]
     }
