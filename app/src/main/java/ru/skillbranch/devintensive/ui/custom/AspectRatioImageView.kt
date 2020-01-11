3a47d33d3f90eaf0988a85891b657c665899ecf2
Yegor Chemisov <yegor.chemisov@gmail.com>
2020-01-11 23:44:47 +0200

    Added DEFAULT_ASPECT_RATIO

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
@@ -2,12 +2,21 @@ package ru.skillbranch.devintensive.ui.custom
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.ImageView
 
-class AspectRatioImageView (
+class AspectRatioImageView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-)
-
+) : ImageView(context, attrs, defStyleAttr)
 {
+    companion object {
+        private const val DEFAULT_ASPECT_RATIO = 1.78f
+    }
+
+    private var aspectRatio = DEFAULT_ASPECT_RATIO
+
+    init {
+
+    }
 }
