96e9db56dabd88c6644de16231ab0a8a21210465
Yegor Chemisov <yegor.chemisov@gmail.com>
2020-01-12 00:28:03 +0200

    Added fun onMeasure

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
@@ -3,6 +3,7 @@ package ru.skillbranch.devintensive.ui.custom
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.ImageView
+import ru.skillbranch.devintensive.R
 
 class AspectRatioImageView @JvmOverloads constructor(
     context: Context,
@@ -17,6 +18,16 @@ class AspectRatioImageView @JvmOverloads constructor(
     private var aspectRatio = DEFAULT_ASPECT_RATIO
 
     init {
+        if (attrs!=null){
+            val a = context.obtainStyledAttributes(attrs, R.styleable.AspectRatioImageView)
+            aspectRatio=a.getFloat(R.styleable.AspectRatioImageView_aspectRatio, DEFAULT_ASPECT_RATIO)
+            a.recycle()
+        }
+    }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val newHeight = (measuredWidth / aspectRatio).toInt()
+        setMeasuredDimension(measuredWidth, newHeight)
     }
 }
