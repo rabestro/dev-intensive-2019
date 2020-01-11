cfe4b9dc2b7ae35908d64b48357e918cc46b46be
Yegor Chemisov <yegor.chemisov@gmail.com>
2020-01-11 23:37:57 +0200

    Added AspectRatioImageView class

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
@@ -0,0 +1,13 @@
+package ru.skillbranch.devintensive.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+
+class AspectRatioImageView (
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+)
+
+{
+}
